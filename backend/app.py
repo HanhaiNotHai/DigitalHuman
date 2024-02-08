7d77c975831191db0f72524690651bfb5560ed89
Hanhai <1932232849@qq.com>
2024-02-08 13:29:08 +0800

    Init project

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1>"
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
