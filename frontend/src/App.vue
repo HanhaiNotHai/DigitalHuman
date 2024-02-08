cff043085be2ddfe705c4817a181e40063374022
Hanhai <85443834+HanhaiNotHai@users.noreply.github.com>
2024-02-08 13:32:14 +0800

    Init project (#1)

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style></style>
